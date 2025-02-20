52396f2ef4020cff5cec8d98b01aa2287d283846
Msa-soilihi-Hamza <hamza.msa-soilihi@laplateforme.io>
2025-02-20 10:02:14 +0100

    connexion

--- back/src/Security/CorsAuthenticator.php
+++ back/src/Security/CorsAuthenticator.php
@@ -2,20 +2,21 @@
 
 namespace App\Security;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 
 class CorsAuthenticator extends AbstractAuthenticator
 {
     public function supports(Request $request): ?bool
     {
-        return $request->isMethod('OPTIONS') && $request->headers->has('Origin');
+        return $request->isMethod('OPTIONS');
     }
 
     public function authenticate(Request $request): Passport
@@ -30,6 +31,6 @@ class CorsAuthenticator extends AbstractAuthenticator
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        return null;
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 } 
